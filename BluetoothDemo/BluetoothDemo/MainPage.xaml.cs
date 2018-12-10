0c7fb3cf2f8e69d83b3e3b3d8e402b9f2ba7bdba
Rakesh Tripathi <Rakesh.Tripathi@t4g.com>
2018-12-10 16:22:52 -0400

    Add project files.

---
+++ BluetoothDemo/BluetoothDemo/MainPage.xaml.cs
@@ -0,0 +1,89 @@
+﻿using Plugin.BLE;
+using Plugin.BLE.Abstractions.Contracts;
+using Plugin.BLE.Abstractions.Exceptions;
+using System;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using Xamarin.Forms;
+
+namespace BluetoothDemo
+{
+    public partial class MainPage : ContentPage
+
+    {
+        IAdapter adapter;
+        IBluetoothLE bluetoothBLE;
+        ObservableCollection<IDevice> list;
+        IDevice device;
+
+
+         
+        public MainPage()
+        {
+            InitializeComponent();
+
+            bluetoothBLE = CrossBluetoothLE.Current;
+            adapter = CrossBluetoothLE.Current.Adapter;
+
+
+
+            list = new ObservableCollection<IDevice>();
+            DevicesList.ItemsSource = list;
+
+        }
+
+        private async void searchDevice(object sender, EventArgs e)
+        {
+            if (bluetoothBLE.State == BluetoothState.Off)
+            {
+                await DisplayAlert("Atenção", "Bluetooth desabilitado.", "OK");
+            }
+            else
+            {
+                list.Clear();
+
+                adapter.ScanTimeout = 10000;
+                adapter.ScanMode = ScanMode.Balanced;
+
+
+                adapter.DeviceDiscovered += (obj, a) =>
+                {
+                    if (a.Device.Name!=null)
+                    {
+                        list.Add(a.Device);
+                    }
+                };
+
+                await adapter.StartScanningForDevicesAsync();
+
+            }
+
+        }
+
+        private async void DevicesList_OnItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            device = DevicesList.SelectedItem as IDevice;
+
+            var result = await DisplayAlert("AVISO", "Deseja se conectar a esse dispositivo?", "Conectar", "Cancelar");
+
+            if (!result)
+                return;
+
+            //Stop Scanner
+            await adapter.StopScanningForDevicesAsync();
+
+            try
+            {
+                await adapter.ConnectToDeviceAsync(device);
+
+                await DisplayAlert("Conectado", "Status:" + device.State, "OK");
+
+            }
+            catch (DeviceConnectionException ex)
+            {
+                await DisplayAlert("Erro", ex.Message, "OK");
+            }
+
+        }
+    }
+}
